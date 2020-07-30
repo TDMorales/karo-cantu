5788bbaff7c8e61a10bcdb10cdc9e98dcfb1b92c
ezrablack <timothydanielmorales@gmail.com>
2020-07-30 05:42:06 -0500

    first commit added homepage

---
+++ src/components/homepage/homepage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './homepage.scss'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>   
+                <div className='content'>
+                    <h1 className='title'>PHOTOGRAPHY</h1>
+                    <span className='subtilte'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>   
+                <div className='content'>
+                    <h1 className='title'>PINCHES FEELINGS</h1>
+                    <span className='subtilte'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
