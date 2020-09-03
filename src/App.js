5e74fbc38bb5eb1fdc78b4d89b65230d776c1a7f
ezrablack <timothydanielmorales@gmail.com>
2020-09-03 14:36:04 -0500

    about page updated with quote and image

--- src/App.js
+++ src/App.js
@@ -12,15 +12,16 @@ import CheckoutPage from './pages/checkout/checkout.jsx';
 import Header from './components/header/header.jsx'
 import AboutPage from './pages/about/about';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    const {setCurrentUser} = this.props;
+    const {setCurrentUser, collectionsArray} = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -34,6 +35,7 @@ class App extends React.Component {
         });
       }
       setCurrentUser(userAuth);
+      addCollectionAndDocuments('collections', collectionsArray)
     });
   }
 
@@ -58,7 +60,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import karo from '../../img/karo.jpg'
+
+import './about.scss';
 
 const AboutSection = () => (
-    <div>
-        <h1> HI THERE ITS ME KARO</h1>
+    <div className='about'>
+        <img className='about-image' src={karo} alt='hero'/>
+        <h1>KARO CANTÚ</h1>
+        <p>Photography is my main medium. I draw, too. But my drawings are more like my diary. A big inspiration for my work has always been a vulnerability and my own community. I grew up in Mexico and permanently moved to the States when I was 17 years old, so the culture shock was real. I seriously thought my life would be a Lizzie McGuire episode until it wasn’t. It was hard to fit in. Getting used to it was hard. This country made me a very emotional person, and I found myself constantly comparing myself to others instead of just creating. Bottling up my feelings and insecurities was getting old. Embracing that I am (and will never be) perfect made me grow as a creative.</p>
     </div>
 )
 
